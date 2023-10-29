16e469e574bd972e75dff40123d154317b781bd5
kabandapatrick2580 <sendsome2019@gmail.com>
2023-10-29 22:29:44 +0200

    1-print_square.js fixed

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,6 +6,7 @@
     of the program.
 */
 
+// Check if there are at least two command-line arguments (including the script name).
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
@@ -13,13 +14,16 @@ if (process.argv.length <= 2) {
     process.exit(1);
 }
 
-const size = parseInt(process.argv[2], 10); // Parse the size argument with base 10 (decimal)
+// Parse the size argument as an integer.
+const size = parseInt(process.argv[2]);
 
+// Check if the parsed size is a valid positive integer.
 if (isNaN(size) || size < 1) {
     process.stderr.write("Invalid size. Please provide a positive integer.\n");
     process.exit(1);
 }
 
+// Loop to print the square of "#" characters.
 for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
         process.stdout.write("#");
